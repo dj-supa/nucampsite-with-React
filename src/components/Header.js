44ed60222403ae00aa727ab1f34e99fc6e74105c
Mario <marioperez_marioperez@yahoo.com>
2022-09-13 16:44:21 -0700

    UserLogin with react

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,7 @@ import {
 } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 import NucampLogo from '../app/assets/img/logo.png';
+import UserLoginForm from '../features/user/userLoginForm';
 
 
 const Header = () => {
@@ -46,6 +47,7 @@ const Header = () => {
                         </NavLink>
                     </NavItem>
                 </Nav>
+                <UserLoginForm/>
             </Collapse>
         </Navbar>
     )
