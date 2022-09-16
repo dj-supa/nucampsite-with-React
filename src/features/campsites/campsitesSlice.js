edadf57067818d819826d77916248c6ab3c88220
Mario <marioperez_marioperez@yahoo.com>
2022-09-15 19:04:26 -0700

    Async Campsites

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,14 +1,46 @@
-import { createSlice} from '@reduxjs/toolkit';
-import { CAMPSITES } from '../../app/shared/CAMPSITES';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { act } from 'react-dom/test-utils';
+//import { CAMPSITES } from '../../app/shared/CAMPSITES';
+import { baseUrl } from '../../app/shared/baseUrl';
+import { mapImageURL } from '../../utils/mapImageURL';
+
+export const fetchCampsites = createAsyncThunk(
+    'campsites/fetchCampsites',
+    async () => {
+        const response = await fetch(baseUrl + 'campsites');
+        if (!response.ok) {
+            return Promise.reject('Unable to fetch, status: ' + response.status);
+        }
+        const data = await response.json();
+        return data;
+    }
+);
 
 const initialState = {
-    campsitesArray: CAMPSITES
+    campsitesArray: [],
+    isLoading: true,
+    errMsg: ''
 };
 
 const campsitesSlice = createSlice({
     name: 'campsites',
-    initialState
-})
+    initialState,
+    reducers: {},
+    extraReducers: {
+        [fetchCampsites.pending]: (state) => {
+            state.isLoading = true;
+        },
+        [fetchCampsites.fulfilled]: (state, action) => {
+            state.isLoading = false;
+            state.errMsg = '';
+            state.campsitesArray = mapImageURL(action.payload);
+        },
+        [fetchCampsites.rejected]: (state, action) => {
+            state.isLoading = false;
+            state.errMsg = action.error ? action.error.message : 'Fetch failed';
+        }
+    }
+});
 
 export const campsitesReducer = campsitesSlice.reducer;
 
@@ -20,7 +52,7 @@ export const selectALLCampsites = (state) => {
 //     return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
 // }
 
-export const selectCampsiteById= (Id) => (state) => {
+export const selectCampsiteById = (Id) => (state) => {
     return state.campsites.campsitesArray.find(
         (campsite) => campsite.id === parseInt(Id));
 };
